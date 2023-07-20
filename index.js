2777d81bd61863e6de46ffb7038ddd4cfe6437c3
Snowlyst <tclam.snow@gmail.com>
2023-07-20 09:53:33 +0800

    working backend

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:index", async (req, res) => {
+  const sightings = await getSightings();
+  const index = req.params.index;
+  res.json(sightings[index]);
 });
 
 app.listen(PORT, () => {
